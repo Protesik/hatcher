48e31e744c3059501ad0d181cfa0d3fd7d87004b
Protesik <protesik@mail.ru>
2022-09-03 23:38:25 +0300

    last 03.09

---
+++ Change1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+void visu1(int b)
+{
+    int a =1;
+    cout << "Jacc =" << a+b <<endl;
+    
+}
+
+int main()
+{
+    cout << 6 %8 <<endl;
+    visu1 (2);
+    return 0;
+}
