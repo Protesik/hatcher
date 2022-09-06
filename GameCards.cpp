5c3963eeec189a5e0646074677b758d657310586
Protesik <protesik@mail.ru>
2022-09-06 13:23:46 +0300

    13:23

--- GameCards.cpp
+++ GameCards.cpp
@@ -5,6 +5,7 @@
 #include <algorithm> 
 #include <stdlib.h>
 #include <time.h>
+#include <vector>
 
 using namespace std;
 
@@ -14,22 +15,22 @@ struct for_kart
 {
     int number;
     int mast;
-    int status;
 };
 
 struct otchet
 {
-    int koloda;
-    int player1;
-    int player2;
-    int kozur_basa;
+    int koloda[36];
+    int player1 = 6;
+    int player2 = 6;
+    int kozur_basa = 0;
+    int KolodaKolvo = 36;
+    for_kart InitCards[36];
 };
 
 struct PlayersCards
 {
-    int *player1 = new int [6];
-    int *player2 = new int [6];
-    int *koloda = new int [36];
+    vector<int> player1;
+    vector<int> player2;
 };
 
 enum ConsoleColor
@@ -74,26 +75,49 @@ bool kartStart()
     return (0);
 }
 
-void shuffle (PlayersCards *AllCards1)
+void shuffle (otchet *OtchetFull)
 {
     int temp = 0;
     int randomIndex = 0;
     for (int i=0; i<36; i++)
         {
             randomIndex = rand() % 36;
-            temp = (AllCards1->koloda[i]);
-            AllCards1->koloda[i] = AllCards1->koloda[randomIndex];
-            AllCards1->koloda[randomIndex] = temp;
+            temp = (OtchetFull->koloda[i]);
+            OtchetFull->koloda[i] = OtchetFull->koloda[randomIndex];
+            OtchetFull->koloda[randomIndex] = temp;
         }
 }
 
-void InitFirstCardsForPlayers(PlayersCards *AllCards)
+void InitFirstCardsForPlayers(otchet *OtchetFull)
 {
-    /*for (int i=0; i<6; i++)
+    int i = 0;
+    OtchetFull->kozur_basa = rand() % 4;
+    OtchetFull->player1 = 6;
+    OtchetFull->player2 = 6;
+    while (i<36)
         {
-            AllCards->player1[i] = AllCards->koloda[i];
-            AllCards->player2[i] = AllCards->koloda[i+6];
-        }*/
+            for (int j = 0; j < 4; j++)
+                {
+                    OtchetFull->koloda[i] = i;
+                    OtchetFull->InitCards[i].number = i;
+                    OtchetFull->InitCards[i].mast = j;
+                    i++; 
+                }
+        }
+}
+
+void CheckCardOfPlayers(otchet *OtchetFull, PlayersCards *AllCards, int P1, int P2)
+{
+    for (int i = 0; i < OtchetFull->player1; i++)
+        {
+            AllCards->player1[i] = OtchetFull->koloda[OtchetFull->KolodaKolvo-i];
+
+        }
+    for (int i = 0; i < OtchetFull->player2; i++)
+        {
+            AllCards->player1[i] = OtchetFull->koloda[OtchetFull->KolodaKolvo-i];
+
+        }
 }
 
 void GameLog()
@@ -105,24 +129,21 @@ void GameLog()
 void StartSettingGame(otchet *OtchetFull, PlayersCards *AllCards )
 {
     int i;
-    srand(time(NULL));
-    OtchetFull->kozur_basa = rand() % 4;
-    for (i = 0; i < 36; i++)
-        AllCards->koloda[i] = i;
-    shuffle(AllCards);
-    for (i = 0; i < 6; i++)
-        {
-            AllCards->player1[i] = AllCards->koloda[36-i];
-            AllCards->player2[i] = AllCards->koloda[36-i-6];
-        }
+    InitFirstCardsForPlayers(OtchetFull);
+    /*for (i=0;i<36;i++)
+        cout << "Second : " << OtchetFull->InitCards[i].number << endl;*/
+    shuffle(OtchetFull);
+    CheckCardOfPlayers(OtchetFull, AllCards, 0, 0);
     
 }
 
 void kartu1()
 {
-    otchet OtchetFull = {36,6,6,0};
+    int P1Need = 0, P2Need = 0;
+    otchet OtchetFull;
     PlayersCards AllCards;
     StartSettingGame(&OtchetFull, &AllCards);
+    cout <<OtchetFull.koloda[25]<< endl;
     
 
 }
@@ -130,6 +151,23 @@ void kartu1()
 
 int main ()
 {
-    kartu1();
+    //srand(time(NULL));
+    int yy = 56;
+    vector<int> imm;
+    for (int i =0; i<5; i++)
+        {
+        imm.push_back(i);
+        cout << imm[i] << " pervui\n";
+        }
+    cout << "\nFirst Size " << imm.size();
+    imm.push_back(yy);
+    cout << "\nSecond Size " << imm.size();
+    for ( int i = 0; i < imm.size(); i++)
+        cout << "\n vtoru " << imm[i];
+    //int ikk[3] = {3,2,1};
+    //imm.insert(ikk);
+    //for ( int i = 0; i < imm.size(); i++)
+        //cout << "\n tri " <<imm[i];
+    //kartu1();
     return 0;
 }
